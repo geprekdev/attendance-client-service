d0acaf9b25255d79f3476b521e7670be3e4b2f26
ujklm23 <ridwanrasyid345@gmail.com>
2022-06-25 23:49:35 +0700

    Fix StudentAccount.js

--- src/student/StudentAttendance.js
+++ src/student/StudentAttendance.js
@@ -1,40 +1,41 @@
 import Layout from "../components/Layout";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { Icon } from "@mdi/react";
-import L from 'leaflet';
+import L from "leaflet";
 import { Link } from "react-router-dom";
 import { mdiArrowLeft } from "@mdi/js";
 import "./App.css";
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 export default function StudentAttendance() {
-let DefaultIcon = L.icon({
+  let DefaultIcon = L.icon({
     iconUrl: icon,
-    shadowUrl: iconShadow
-});
-  L.Marker.prototype.options.icon = DefaultIcon
+    shadowUrl: iconShadow,
+  });
+
+  L.Marker.prototype.options.icon = DefaultIcon;
   const currentLocation = [6.1, -5.2];
   return (
     // ----
     <Layout title="Student Attendance" role="STUDENT">
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
-      integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
-      crossorigin=""
-    />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+        integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+        crossorigin=""
+      />
       <div className="relative mx-auto h-screen max-w-[444px] border pb-24 shadow-lg">
-	<Link to="/student">        
-<span className="absolute left-4 top-3 z-20 rounded-full bg-white p-[5px] shadow-lg">
-          <Icon path={mdiArrowLeft} size="25px" />
-        </span>
-	</Link>
+        <Link to="/student">
+          <span className="absolute left-4 top-3 z-20 rounded-full bg-white p-[5px] shadow-lg">
+            <Icon path={mdiArrowLeft} size="25px" />
+          </span>
+        </Link>
         <div className="z-1 absolute h-[70%] w-full">
           <MapContainer
             center={currentLocation}
             zoom={13}
-	    zoomControl={false}
+            zoomControl={false}
             scrollWheelZoom={false}
           >
             <TileLayer
